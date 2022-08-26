40cbccb3df9fd72627649ad653ba11c4503359d5
amedeomajer <apemajer@gmail.com>
2022-08-26 16:46:34 +0300

    cbfduibv

--- part2/dataforcountries/src/components/SearchCountrie.js
+++ part2/dataforcountries/src/components/SearchCountrie.js
@@ -1,37 +1,67 @@
 import { useState } from "react"
 
+const CountryCard = ({country, handleClick}) => {
+	console.log(country)
+	const languagesList = Object.values(country.languages).map(language => <li key={language}>{language}</li>)
+	
+	return (
+		<div>
+			<h1>{country.name.common}</h1>
+			<p>Capital: {country.capital[0]}</p>
+			<p>Population: {country.population}</p>
+			<img src={country.flags.png} alt={country.name.common}/>
+			<h2>Languages</h2>
+			<ul>
+				{languagesList}
+			</ul>
+		</div>
+	)
+}
+
+const Result = ({countries, handleClick}) => {
+	if (countries.length < 11 && countries.length !== 1) {
+		return countries.map(country => <li onClick={handleClick(country.name.common)} key={country.name.common} >{country.name.common}</li>)
+		} else if (countries.length > 10) {
+		return <p>too many matches, be more specific</p>
+	} else if (countries.length === 1) {
+		return <CountryCard country={countries[0]} />
+	}
+}
+
+
 const SearchCountry = ({countries}) => {
 
 	const [searchResult, setSearchResult] = useState([])
+
+	const handleClick = (selected) => {
+		setSearchResult(selected);
+	}
 
 	const searchCountry = (event) => {
 		const name = event.target.value;
 		let result = countries
 			.filter(country => country.name.common.toLowerCase()
 			.includes(name.toLowerCase()));
-		console.log('this is results', result, 'result is an object!')
-		console.log(result[0]);
+		
+		setSearchResult(result);
+		if (event.target.value === "") {
+			setSearchResult([])
+		}
 	}
 
 
 	return (
-		<form>
-			search country: <input onChange={searchCountry}/>
-		</form>
+		<div>
+			<form>
+				search country: <input onChange={searchCountry}/>
+				<br></br>
+			</form>
+			search result:
+			<ul>
+				<Result countries={searchResult} handleClick={handleClick}/>
+			</ul>
+		</div>
 	)
 }
 
-export default SearchCountry
-
-/*
-<h1>{country.name}</h1>
-br
-<p>Capital: {country.capital}</p>
-<p>Area: {country.area}</p>
-br
-<h3>languages</h3>
-<ul>
-	<li>{country.languages}</li>
-</ul>
-<img src='url("country.flag")'>
-*/ 
+export default SearchCountry;
